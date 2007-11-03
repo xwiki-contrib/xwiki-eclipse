ed4ed3c3fd02bfb580c84b8a3142688ccb78907d
fmancinelli <fabio.mancinelli@xwiki.com>
2007-11-03 12:52:08 +0000

    XECLIPSE-30
    * Added remove page/space menu items in standalone RPC application
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-extensions/xwiki-eclipse/trunk@5622 f329d543-caf0-0310-9063-dda96c69346f

--- plugins/org.xwiki.xeclipse.rcp/src/main/java/org/xwiki/xeclipse/rcp/ApplicationActionBarAdvisor.java
+++ plugins/org.xwiki.xeclipse.rcp/src/main/java/org/xwiki/xeclipse/rcp/ApplicationActionBarAdvisor.java
@@ -1,8 +1,10 @@
 package org.xwiki.xeclipse.rcp;
 
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ContributionItemFactory;
 import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
@@ -31,6 +33,9 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
         
         IWorkbenchAction showViewMenuAction = ActionFactory.SHOW_VIEW_MENU.create(window);
         register(showViewMenuAction);        
+        
+        IContributionItem item = ContributionItemFactory.VIEWS_SHORTLIST.create(window);
+        
     }
 
     protected void fillMenuBar(IMenuManager menuBar) {          
