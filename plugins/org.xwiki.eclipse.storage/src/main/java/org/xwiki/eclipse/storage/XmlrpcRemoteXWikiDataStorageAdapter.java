96f761affcad1e4cde6d92605f0a827e5e6c715a
Jun Han <jun.han37@gmail.com>
2011-07-03 23:22:12 -0400

    [xeclipse] update xmlrpc adapter class to implement the getPage, getWikis(), and getPageHistory() method in order to incorporate the newly added attributes in the model classes

--- plugins/org.xwiki.eclipse.storage/src/main/java/org/xwiki/eclipse/storage/XmlrpcRemoteXWikiDataStorageAdapter.java
+++ plugins/org.xwiki.eclipse.storage/src/main/java/org/xwiki/eclipse/storage/XmlrpcRemoteXWikiDataStorageAdapter.java
@@ -20,13 +20,15 @@
 package org.xwiki.eclipse.storage;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.codehaus.swizzle.confluence.ServerInfo;
 import org.codehaus.swizzle.confluence.SpaceSummary;
-import org.xwiki.eclipse.model.ModelObject;
 import org.xwiki.eclipse.model.XWikiEclipseAttachment;
 import org.xwiki.eclipse.model.XWikiEclipseObjectSummary;
+import org.xwiki.eclipse.model.XWikiEclipsePage;
+import org.xwiki.eclipse.model.XWikiEclipsePageHistorySummary;
 import org.xwiki.eclipse.model.XWikiEclipsePageSummary;
 import org.xwiki.eclipse.model.XWikiEclipseServerInfo;
 import org.xwiki.eclipse.model.XWikiEclipseSpaceSummary;
@@ -34,6 +36,7 @@ import org.xwiki.eclipse.model.XWikiEclipseWikiSummary;
 import org.xwiki.eclipse.xmlrpc.XWikiEclipseXmlrpcException;
 import org.xwiki.eclipse.xmlrpc.XmlrpcRemoteXWikiDataStorage;
 import org.xwiki.xmlrpc.model.XWikiObjectSummary;
+import org.xwiki.xmlrpc.model.XWikiPageHistorySummary;
 import org.xwiki.xmlrpc.model.XWikiPageSummary;
 
 /**
@@ -64,45 +67,28 @@ public class XmlrpcRemoteXWikiDataStorageAdapter implements IRemoteXWikiDataStor
     }
 
     /**
-     * {@inheritDoc}
-     * 
-     * @see org.xwiki.eclipse.storage.IRemoteXWikiDataStorage#getRootResources()
-     */
-    @Override
-    public List<ModelObject> getRootResources()
-    {
-        List<ModelObject> result = new ArrayList<ModelObject>();
-        List<SpaceSummary> spaces;
-        try {
-            spaces = this.xmlrpcRemoteDataStorage.getSpaces();
-            for (SpaceSummary spaceSummary : spaces) {
-                XWikiEclipseSpaceSummary space = new XWikiEclipseSpaceSummary(dataManager);
-                space.setKey(spaceSummary.getKey());
-                space.setName(spaceSummary.getName());
-                space.setUrl(spaceSummary.getUrl());
-                space.setWiki("xwiki");
-
-                result.add(space);
-            }
-            return result;
-
-        } catch (XWikiEclipseXmlrpcException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
+     * as xmlrpc can only process one return a single xwiki instance {@inheritDoc}
      * 
      * @see org.xwiki.eclipse.storage.IRemoteXWikiDataStorage#getWikis()
      */
     @Override
     public List<XWikiEclipseWikiSummary> getWikis()
     {
-        return null;
+        List<XWikiEclipseWikiSummary> result = new ArrayList<XWikiEclipseWikiSummary>();
+        XWikiEclipseWikiSummary xwiki = new XWikiEclipseWikiSummary(dataManager);
+
+        XWikiEclipseServerInfo serverInfo = getServerInfo();
+
+        xwiki.setBaseUrl(serverInfo.getBaseUrl());
+        xwiki.setName("xwiki"); // by default, it is xwiki
+        xwiki.setSpacesUrl("N/A"); // by default, it does not know about rest urls
+        xwiki.setSyntaxes(serverInfo.getSyntaxes());
+        xwiki.setVersion(serverInfo.getVersion());
+        xwiki.setWikiId("xwiki"); // by default, it is xwiki
+
+        result.add(xwiki);
+
+        return result;
     }
 
     /**
@@ -160,6 +146,14 @@ public class XmlrpcRemoteXWikiDataStorageAdapter implements IRemoteXWikiDataStor
             serverInfo.setBaseUrl(info.getBaseUrl());
             serverInfo.setMajorVersion(info.getMajorVersion());
             serverInfo.setMinorVersion(info.getMinorVersion());
+            /* set up the version string */
+            String version = info.getMajorVersion() + "." + info.getMinorVersion();
+            serverInfo.setVersion(version);
+
+            /* by default, use xwiki/1.0 as the syntaxes */
+            List<String> syntaxes = new ArrayList<String>();
+            syntaxes.add("xwiki/1.0");
+            serverInfo.setSyntaxes(syntaxes);
 
             return serverInfo;
         } catch (XWikiEclipseXmlrpcException e) {
@@ -185,12 +179,17 @@ public class XmlrpcRemoteXWikiDataStorageAdapter implements IRemoteXWikiDataStor
             for (XWikiPageSummary pageSummary : pages) {
                 XWikiEclipsePageSummary page = new XWikiEclipsePageSummary(dataManager);
                 page.setId(pageSummary.getId());
+                page.setName(pageSummary.getId());
+                String fullName = pageSummary.getSpace() + "." + pageSummary.getId();
+                page.setFullName(fullName);
                 page.setParentId(pageSummary.getParentId());
                 page.setSpace(pageSummary.getSpace());
                 page.setTitle(pageSummary.getTitle());
                 page.setUrl(pageSummary.getUrl());
                 /* default value is "xwiki" for all xmlrpc implementation */
                 page.setWiki("xwiki");
+                page.setTranslations(pageSummary.getTranslations());
+                page.setSyntax("xwiki/1.0");
 
                 result.add(page);
             }
@@ -240,6 +239,56 @@ public class XmlrpcRemoteXWikiDataStorageAdapter implements IRemoteXWikiDataStor
     @Override
     public List<XWikiEclipseAttachment> getAttachments(XWikiEclipsePageSummary pageSummary)
     {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.eclipse.storage.IRemoteXWikiDataStorage#getPageHistory(org.xwiki.eclipse.model.XWikiEclipsePageSummary)
+     */
+    @Override
+    public List<XWikiEclipsePageHistorySummary> getPageHistory(XWikiEclipsePageSummary pageSummary)
+        throws XWikiEclipseStorageException
+    {
+        List<XWikiEclipsePageHistorySummary> result = new ArrayList<XWikiEclipsePageHistorySummary>();
+
+        try {
+            List<XWikiPageHistorySummary> history = xmlrpcRemoteDataStorage.getPageHistory(pageSummary.getId());
+            for (XWikiPageHistorySummary historySummary : history) {
+                XWikiEclipsePageHistorySummary h = new XWikiEclipsePageHistorySummary(dataManager);
+                h.setLanguage("en");
+                h.setMajorVersion(historySummary.getVersion());
+                h.setMinorVersion(historySummary.getMinorVersion());
+
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(historySummary.getModified());
+                h.setModified(cal);
+
+                h.setModifier(historySummary.getModifier());
+                h.setPageId(historySummary.getBasePageId());
+
+                result.add(h);
+                return result;
+            }
+        } catch (XWikiEclipseXmlrpcException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw new XWikiEclipseStorageException(e);
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.eclipse.storage.IRemoteXWikiDataStorage#getPage(org.xwiki.eclipse.model.XWikiEclipsePageHistorySummary)
+     */
+    @Override
+    public XWikiEclipsePage getPage(XWikiEclipsePageHistorySummary pageHistorySummary)
+    {
+        // TODO Auto-generated method stub
         return null;
     }
 
