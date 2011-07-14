d5a0f39346af3fa070778f75cedcd0794aed5ac0
Jun Han <jun.han37@gmail.com>
2011-07-14 15:29:27 -0400

    [xeclipse] add progress monitor when deleting and comment out all the delete operations that are not checked yet

--- plugins/org.xwiki.eclipse.ui/src/main/java/org/xwiki/eclipse/ui/handlers/DeleteXWikiElementHandler.java
+++ plugins/org.xwiki.eclipse.ui/src/main/java/org/xwiki/eclipse/ui/handlers/DeleteXWikiElementHandler.java
@@ -26,8 +26,12 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.SafeRunner;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -38,6 +42,8 @@ import org.xwiki.eclipse.model.XWikiEclipseObjectSummary;
 import org.xwiki.eclipse.model.XWikiEclipsePageSummary;
 import org.xwiki.eclipse.model.XWikiEclipseSpaceSummary;
 import org.xwiki.eclipse.storage.DataManager;
+import org.xwiki.eclipse.storage.notification.CoreEvent;
+import org.xwiki.eclipse.storage.notification.NotificationManager;
 import org.xwiki.eclipse.ui.dialogs.SelectionDialog;
 import org.xwiki.eclipse.ui.utils.UIUtils;
 import org.xwiki.eclipse.ui.utils.XWikiEclipseSafeRunnable;
@@ -47,11 +53,16 @@ import org.xwiki.eclipse.ui.utils.XWikiEclipseSafeRunnable;
  */
 public class DeleteXWikiElementHandler extends AbstractHandler
 {
+    /* only fire the remove event once, which is after the last object is deleted */
+    static Object toBeRefreshed = null;
+
+    static CoreEvent.Type coreEvent = null;
+
     public Object execute(ExecutionEvent event) throws ExecutionException
     {
         ISelection selection = HandlerUtil.getCurrentSelection(event);
 
-        Set selectedObjects = UIUtils.getSelectedObjectsFromSelection(selection);
+        final Set selectedObjects = UIUtils.getSelectedObjectsFromSelection(selection);
 
         SelectionDialog selectionDialog =
             new SelectionDialog(HandlerUtil.getActiveShell(event), "Delete objects",
@@ -61,91 +72,125 @@ public class DeleteXWikiElementHandler extends AbstractHandler
             return null;
         }
 
-        Set<Object> objectsToBeRemoved = selectionDialog.getSelectedObjects();
+        final Set<Object> objectsToBeRemoved = selectionDialog.getSelectedObjects();
 
-        for (Object selectedObject : selectedObjects) {
-            if (objectsToBeRemoved.contains(selectedObject)) {
-                if (selectedObject instanceof XWikiEclipsePageSummary) {
-                    final XWikiEclipsePageSummary pageSummary = (XWikiEclipsePageSummary) selectedObject;
+        Job deleteJob = new Job("Deleting...")
+        {
 
-                    SafeRunner.run(new XWikiEclipseSafeRunnable()
-                    {
-                        public void run() throws Exception
-                        {
-                            pageSummary.getDataManager().removePage(pageSummary);
+            @Override
+            protected IStatus run(final IProgressMonitor monitor)
+            {
+
+                monitor.beginTask("Downloading", 100);
+                if (monitor.isCanceled()) {
+                    return Status.CANCEL_STATUS;
+                }
+                final int work = 100 / objectsToBeRemoved.size();
+
+                for (Object selectedObject : selectedObjects) {
+                    if (objectsToBeRemoved.contains(selectedObject)) {
+                        if (selectedObject instanceof XWikiEclipsePageSummary) {
+                            final XWikiEclipsePageSummary pageSummary = (XWikiEclipsePageSummary) selectedObject;
+
+                            // SafeRunner.run(new XWikiEclipseSafeRunnable()
+                            // {
+                            // public void run() throws Exception
+                            // {
+                            // pageSummary.getDataManager().removePage(pageSummary);
+                            // monitor.worked(work);
+                            // }
+                            // });
+
                         }
-                    });
 
+                        if (selectedObject instanceof XWikiEclipseObjectSummary) {
+                            final XWikiEclipseObjectSummary objectSummary = (XWikiEclipseObjectSummary) selectedObject;
+
+                            // SafeRunner.run(new XWikiEclipseSafeRunnable()
+                            // {
+                            // public void run() throws Exception
+                            // {
+                            // objectSummary.getDataManager().removeObject(objectSummary);
+                            // }
+                            // });
+
+                        }
+
+                        if (selectedObject instanceof DataManager) {
+                            final DataManager dataManager = (DataManager) selectedObject;
+
+                            monitor.setTaskName("Deleting " + dataManager.getName());
+
+                            SafeRunner.run(new XWikiEclipseSafeRunnable()
+                            {
+                                public void run() throws Exception
+                                {
+                                    DataManagerRegistry.getDefault().unregister(dataManager);
+                                    dataManager.getProject().delete(true, null);
+                                    ResourcesPlugin.getWorkspace().save(true, new NullProgressMonitor());
+                                    monitor.worked(work);
+                                    Thread.sleep(2000);
+                                }
+                            });
+
+                        }
+
+                        if (selectedObject instanceof XWikiEclipseSpaceSummary) {
+                            final XWikiEclipseSpaceSummary spaceSummary = (XWikiEclipseSpaceSummary) selectedObject;
+
+                            // SafeRunner.run(new XWikiEclipseSafeRunnable()
+                            // {
+                            // public void run() throws Exception
+                            // {
+                            // spaceSummary.getDataManager().removeSpace(spaceSummary);
+                            // }
+                            // });
+                        }
+
+                        if (selectedObject instanceof XWikiEclipseComment) {
+                            final XWikiEclipseComment comment = (XWikiEclipseComment) selectedObject;
+
+                            /* FIXME: comment delete is not supported in REST API yet */
+                            // SafeRunner.run(new XWikiEclipseSafeRunnable()
+                            // {
+                            // public void run() throws Exception
+                            // {
+                            // comment.getDataManager().removeComment(comment);
+                            // }
+                            // });
+
+                        }
+
+                        if (selectedObject instanceof XWikiEclipseAttachment) {
+                            final XWikiEclipseAttachment attachment = (XWikiEclipseAttachment) selectedObject;
+                            toBeRefreshed = attachment;
+                            coreEvent = CoreEvent.Type.ATTACHMENT_REMOVED;
+                            monitor.setTaskName("Deleting " + attachment.getName());
+
+                            SafeRunner.run(new XWikiEclipseSafeRunnable()
+                            {
+                                public void run() throws Exception
+                                {
+                                    attachment.getDataManager().removeAttachment(attachment);
+                                    monitor.worked(work);
+                                    Thread.sleep(2000);
+                                }
+                            });
+                        }
+                    }
                 }
 
-                if (selectedObject instanceof XWikiEclipseObjectSummary) {
-                    final XWikiEclipseObjectSummary objectSummary = (XWikiEclipseObjectSummary) selectedObject;
+                monitor.done();
 
-                    SafeRunner.run(new XWikiEclipseSafeRunnable()
-                    {
-                        public void run() throws Exception
-                        {
-                            objectSummary.getDataManager().removeObject(objectSummary);
-                        }
-                    });
-
+                if (coreEvent != null) {
+                    NotificationManager.getDefault().fireCoreEvent(coreEvent, this, toBeRefreshed);
                 }
 
-                if (selectedObject instanceof DataManager) {
-                    final DataManager dataManager = (DataManager) selectedObject;
-
-                    SafeRunner.run(new XWikiEclipseSafeRunnable()
-                    {
-                        public void run() throws Exception
-                        {
-                            DataManagerRegistry.getDefault().unregister(dataManager);
-                            dataManager.getProject().delete(true, null);
-                            ResourcesPlugin.getWorkspace().save(true, new NullProgressMonitor());
-                        }
-                    });
-
-                }
-
-                if (selectedObject instanceof XWikiEclipseSpaceSummary) {
-                    final XWikiEclipseSpaceSummary spaceSummary = (XWikiEclipseSpaceSummary) selectedObject;
-
-                    SafeRunner.run(new XWikiEclipseSafeRunnable()
-                    {
-                        public void run() throws Exception
-                        {
-                            spaceSummary.getDataManager().removeSpace(spaceSummary);
-                        }
-                    });
-                }
-
-                if (selectedObject instanceof XWikiEclipseComment) {
-                    final XWikiEclipseComment comment = (XWikiEclipseComment) selectedObject;
-
-                    /* FIXME: comment delete is not supported in REST API yet */
-                    SafeRunner.run(new XWikiEclipseSafeRunnable()
-                    {
-                        public void run() throws Exception
-                        {
-                            comment.getDataManager().removeComment(comment);
-                        }
-                    });
-
-                }
-
-                if (selectedObject instanceof XWikiEclipseAttachment) {
-                    final XWikiEclipseAttachment attachment = (XWikiEclipseAttachment) selectedObject;
-
-                    SafeRunner.run(new XWikiEclipseSafeRunnable()
-                    {
-                        public void run() throws Exception
-                        {
-                            attachment.getDataManager().removeAttachment(attachment);
-                        }
-                    });
-
-                }
+                return Status.OK_STATUS;
             }
-        }
+        };
+        deleteJob.setUser(true);
+        deleteJob.schedule();
 
         return null;
     }
